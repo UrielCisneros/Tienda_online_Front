9b04b4c55c70c0902aaf7129447c8180726e81ca
poday98 <poday736.98@gmail.com>
2020-08-26 00:48:54 -0500

    en proceso

--- src/pages/users/Carrito/shopping_cart.js
+++ src/pages/users/Carrito/shopping_cart.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from 'react-router-dom';
 import clienteAxios from '../../../config/axios';
 import jwt_decode from 'jwt-decode';
 
-import { List, InputNumber, Button, Avatar } from 'antd';
+import { List, InputNumber, Button, Avatar, Spin } from 'antd';
 import {ShoppingCartOutlined, ExportOutlined, DeleteOutlined} from '@ant-design/icons';
 
 const styles ={fontSize: 25};
@@ -22,6 +22,7 @@ const data = [
 
 export default function ShoppingCart() {
 
+
     const token = localStorage.getItem('token');
     var decoded = Jwt(token);
     
@@ -33,7 +34,6 @@ export default function ShoppingCart() {
     }
   }
 
-  console.log(decoded);
 
   const [ carrito, setCarrito ] = useState([]);
   const [ loading, setLoading ] = useState(false);
@@ -51,77 +51,35 @@ export default function ShoppingCart() {
           if(decoded._id===null){
               console.log("no hay codigo");
           }
-                    // setCarrito(res.data.articulos);
+                     setCarrito(res.data.articulos);
                     console.log(res.data);
                     console.log(decoded);
+                    setLoading(false);
         })
         .catch((err) => {
-                    //  setCarrito([]);
+                    setCarrito([]);
                     console.log(err.response);
                     console.log("No hay datos");
+                    setLoading(false);
       });
   }
 
   useEffect(() => {
-  //  obtenerDatosCarrito();
+    obtenerDatosCarrito();
+    setLoading(true);
   }, []);
    
 
     return (
       <div className="container" >
-
+        <Spin size="large" spinning={loading}>
         <h1 className="text-center">Tu carrito de compras</h1>
 
         <div  className="row mx-auto" style={{background: "white"}}>
           <div className="col-lg-12 ">
-            <List
-              itemLayout="horizontal"
-              dataSource={data}
-              renderItem={item => (
+            
+          <h2>Lista o mapeo</h2>
 
-                <List.Item>
-                  <div className="d-none d-lg-block px-2">
-                  {<img src="" width="130" height="150" />}
-                  </div>
-
-                  <div>
-                    <div className="d-flex justify-content-lg-center">
-                      <List.Item.Meta
-                        avatar={<img src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" 
-                        className="d-block d-sm-block d-lg-none"  height="70" width="70"/>}
-                        title={item.title}
-                      />
-                    </div>
-
-                    <div className="d-flex mt-3">
-                      <p>Color: </p>
-                      <p className="px-3">Talla: </p>
-                      <p className="px-3">Talla: </p>
-                    </div>
-
-                    <div className="mt-3 d-flex">
-                      <p><Link to="/">
-                        <ShoppingCartOutlined style={styles}/>Comprar ahora </Link> </p>
-                      <p><Link to="/" className="px-3">
-                        <DeleteOutlined style={styles}/>Eliminar producto</Link></p>
-                      <p><Link  to="/" className="px-3">
-                        <ExportOutlined style={styles}/>Apartar</Link></p>
-                    </div>
-                  </div>
-          
-                  <div className="mt-4 align-items-center px-5">
-                    <InputNumber size="large" min={1} max={100}/>
-                    <p>Stock disponibles</p>
-                  </div>
-
-                  <div className="mt-4 ">
-                    <h1>$10000</h1>
-                  </div>  
-                      
-                </List.Item>
-
-              )}
-            />
 
             <div className="d-flex flex-row-reverse mt-22" >
               <div>
@@ -146,7 +104,10 @@ export default function ShoppingCart() {
 
           </div>
 
-        </div>     
+        </div>
+
+        
+        </Spin>     
       </div>
     )
 }
