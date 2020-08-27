1cc809b82127c22faae4fee31ec6bdfed53dbec2
poday98 <poday736.98@gmail.com>
2020-08-27 00:35:39 -0500

    consultas

---
+++ src/pages/users/Carrito/services/MostrarDatosCarrito.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import clienteAxios from '../../../../config/axios';
+import jwt_decode from 'jwt-decode';
+import { Link} from 'react-router-dom';
+
+import { List, InputNumber, notification, Spin } from 'antd';
+import {ShoppingCartOutlined, ExportOutlined, DeleteOutlined} from '@ant-design/icons';
+
+
+const styles ={fontSize: 25};
+
+export default function MostrarDatosProductos () {
+
+  const [ carrito, setCarrito ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+
+    //toma del token para el usuario
+  const token = localStorage.getItem('token');
+  var decoded = Jwt(token);
+  
+  function Jwt(token) {
+  try {
+    return jwt_decode(token);
+  } catch (e) {
+    return null;
+  }
+}
+
+
+async function obtenerDatosCarrito(){
+    setLoading(true);
+      await clienteAxios
+        .get(`/carrito/${decoded._id}`,{
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `bearer ${token}`
+          }
+      })
+
+        .then((res) => {
+          if(decoded._id===null){
+              console.log("no hay codigo");
+          }
+          
+              setCarrito(res.data.articulos);
+              setLoading(false);
+              if (setCarrito() === undefined) {
+                console.log("No hay productos");
+              }
+
+              console.log(decoded);
+        })
+
+        .catch((err) => {
+          console.log(err);
+          console.log(err.response);
+      });
+  }
+
+
+
+    useEffect(() => {
+      obtenerDatosCarrito();
+      setLoading(true);
+    }, []);
+ 
+    return(
+      <Spin  size="large" spinning={loading} >
+        <div>
+            <carritoList 
+            carrito={carrito} 
+            setLoading={setLoading}
+            />
+        </div>
+      </Spin>
+)
+}
+
+function carritoList (props) {
+  const {carrito} = props;
+
+  return (
+    <div>
+        <List className="p-3"
+                itemLayout="horizontal"
+                size="large"
+                dataSource={carrito.idarticulo}
+                renderItem={carrito => 
+                    <Productos carrito={carrito} />
+                }
+            />
+    </div>
+)
+}
+
+function Productos (props) {
+  const {carrito} = props;
+
+  return(
+
+  <List.Item>
+    <div className="d-none d-lg-block px-2">
+      <img
+        width={272}
+        src={`https://prueba-imagenes-uploads.s3.us-west-1.amazonaws.com/${carrito.producto.imagen}`}
+      />
+    </div>
+
+    <div>
+      <div className="d-flex justify-content-lg-center">
+        <List.Item.Meta
+          avatar={<img src={carrito.producto.imagen} 
+          className="d-block d-sm-block d-lg-none"  height="70" width="70"/>}
+          title={carrito.producto.nombre}
+        />
+      </div>
+
+      <div className="d-flex mt-3">
+      {carrito.producto.categoria === 'calzado' ? (
+          <p>
+          
+        </p>
+        ): carrito.producto.categoria === 'ropa' ? (
+        <p>
+        
+        </p>
+        ) : '' }
+      </div>
+
+      <div className="mt-3 d-flex">
+        <p><Link to="/">
+          <ShoppingCartOutlined style={styles}/>Comprar ahora </Link> </p>
+        <p><Link to="/" className="px-3">
+          <DeleteOutlined style={styles}/>Eliminar producto</Link></p>
+        <p><Link  to="/" className="px-3">
+          <ExportOutlined style={styles}/>Apartar</Link></p>
+        </div>
+      </div>
+    
+      <div className="mt-4 align-items-center px-5">
+        <InputNumber size="large" min={1} max={100}/>
+        <p>Stock disponibles</p>
+      </div>
+
+      <div className="mt-4 ">
+        <h1>$10000</h1>
+      </div>  
+                
+  </List.Item>
+
+  )
+}
--- src/pages/users/Carrito/shopping_cart.js
+++ src/pages/users/Carrito/shopping_cart.js
@@ -1,84 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import clienteAxios from '../../../config/axios';
-import jwt_decode from 'jwt-decode';
+import MostrarDatosProductos from './services/MostrarDatosCarrito'
 
-import { List, InputNumber, Button, Avatar, Spin } from 'antd';
-import {ShoppingCartOutlined, ExportOutlined, DeleteOutlined} from '@ant-design/icons';
+import {Button} from 'antd';
+import {ShoppingCartOutlined} from '@ant-design/icons';
 
 const styles ={fontSize: 25};
-const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    
-  ];
+
 
 export default function ShoppingCart() {
 
 
-    const token = localStorage.getItem('token');
-    var decoded = Jwt(token);
-    
-    function Jwt(token) {
-    try {
-      return jwt_decode(token);
-    } catch (e) {
-      return null;
-    }
-  }
-
-
-  const [ carrito, setCarrito ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
-
-  async function obtenerDatosCarrito(){
-    setLoading(true);
-      await clienteAxios
-        .get(`/carrito/${decoded._id}`,{
-          headers: {
-              'Content-Type': 'multipart/form-data',
-              Authorization: `bearer ${token}`
-          }
-      })
-        .then((res) => {
-          if(decoded._id===null){
-              console.log("no hay codigo");
-          }
-                     setCarrito(res.data.articulos);
-                    console.log(res.data);
-                    console.log(decoded);
-                    setLoading(false);
-        })
-        .catch((err) => {
-                    setCarrito([]);
-                    console.log(err.response);
-                    console.log("No hay datos");
-                    setLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    obtenerDatosCarrito();
-    setLoading(true);
-  }, []);
-   
-
     return (
       <div className="container" >
-        <Spin size="large" spinning={loading}>
         <h1 className="text-center">Tu carrito de compras</h1>
 
         <div  className="row mx-auto" style={{background: "white"}}>
           <div className="col-lg-12 ">
             
-          <h2>Lista o mapeo</h2>
+          <MostrarDatosProductos></MostrarDatosProductos>
 
 
             <div className="d-flex flex-row-reverse mt-22" >
@@ -105,9 +45,6 @@ export default function ShoppingCart() {
           </div>
 
         </div>
-
-        
-        </Spin>     
       </div>
     )
 }
