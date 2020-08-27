799a8e74a00f342c4b5914b3b52c0d205f130c9a
poday98 <poday736.98@gmail.com>
2020-08-27 13:46:46 -0500

    crearVista

---
+++ src/pages/users/Datos_tienda/datos_tienda.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import clienteAxios from '../../../config/axios';
+import {Spin} from 'antd'
+
+
+export default function datos_tienda() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
