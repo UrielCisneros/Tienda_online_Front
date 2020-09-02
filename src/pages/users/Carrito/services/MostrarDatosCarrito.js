a6c9355e85d98b3ae7b2eea85067b06ab7204b67
poday98 <poday736.98@gmail.com>
2020-09-02 18:18:17 -0500

    avance

--- src/pages/users/Carrito/services/MostrarDatosCarrito.js
+++ src/pages/users/Carrito/services/MostrarDatosCarrito.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import clienteAxios from '../../../../config/axios';
 import jwt_decode from 'jwt-decode';
 import { Link} from 'react-router-dom';
+import "./carrito.scss";
 
 import { List, InputNumber, notification, Spin } from 'antd';
 import {ShoppingCartOutlined, ExportOutlined, DeleteOutlined} from '@ant-design/icons';
@@ -12,6 +13,8 @@ const styles ={fontSize: 25};
 export default function MostrarDatosProductos () {
 
   const [ carrito, setCarrito ] = useState([]);
+  const [ cliente, setCliente ] = useState([]);
+  const [ contador, setContador ] = useState(0);
   const [ loading, setLoading ] = useState(false);
 
     //toma del token para el usuario
@@ -27,130 +30,134 @@ export default function MostrarDatosProductos () {
 }
 
 
-async function obtenerDatosCarrito(){
+  async function obtenerDatosCarrito(){
     setLoading(true);
-      await clienteAxios
-        .get(`/carrito/${decoded._id}`,{
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            Authorization: `bearer ${token}`
-          }
+    await clienteAxios
+      .get(`/carrito/${decoded._id}`,{
+        headers: {
+        'Content-Type': 'multipart/form-data',
+          Authorization: `bearer ${token}`
+        }
       })
 
-        .then((res) => {
-          if(decoded._id===null){
-              console.log("no hay codigo");
-          }
-            setCarrito(res.data.articulos);
-            setLoading(false);
-            //  if (setCarrito() === undefined) {
-              //  console.log("No hay productos");
-              //}
+      .then((res) => {
+      if(decoded._id===null){
+        console.log("no hay codigo");
+      }
+        
+        setContador(res.data.articulos.length);
+        setCliente(res.data.cliente);
+        setCarrito(res.data);
+        setLoading(false);
+        console.log(res.data.cliente.nombre);
+      })
 
-           // console.log(decoded);
-            console.log(res.data.articulos);
-        })
-
-        .catch((err) => {
-          console.log(err);
-          console.log(err.response);
+      .catch((err) => {
+        console.log(err);
+        console.log(err.response);
       });
   }
 
 
 
-    useEffect(() => {
-      obtenerDatosCarrito();
-      setLoading(true);
-    }, []);
+  useEffect(() => {
+    obtenerDatosCarrito();
+    setLoading(true);
+  }, []);
  
-    return(
-      <Spin  size="large" spinning={loading} >
+  return(
+    <Spin  size="large" spinning={loading} >
+      <h1 className="principal bg-dark">Bievenido {cliente.nombre}</h1>
+    <div>
+
+    
+
+      <List className="p-3"
+            itemLayout="horizontal"
+            size="large"
+            dataSource={carrito.articulos}
+            renderItem={carrit => 
+              <Productos carrito={carrit} />
+            }
+      />
+
+      <div className="d-flex flex-row-reverse mr-2" >
         <div>
-        <List className="p-3"
-                itemLayout="horizontal"
-                size="large"
-                dataSource={carrito.articulos}
-                renderItem={carrit => 
-                    <Productos carrito={carrit} />
-                }
-            />
+          <h2>MilOchoMil</h2>
+          <h2>Gratis</h2>
+          <h1>$1001</h1>
+          <br/><br/>
         </div>
-      </Spin>
-)
+        <div className="px-5 ">
+          <h2>Productos ({contador}) </h2>
+          <Link to="/"><h2>Envio a: </h2></Link>
+          <h2 className="mt-4">Total: </h2>
+        </div>
+      </div>
+    </div> 
+    </Spin>
+  )
 }
 
-// function carritoList (props) {
-//   const {carrito} = props;
 
-//   return (
-//     <div>
-//         <List className="p-3"
-//                 itemLayout="horizontal"
-//                 size="large"
-//                 dataSource={carrito}
-//                 renderItem={carrito => 
-//                     <Productos carrito={carrito} />
-//                 }
-//             />
-//     </div>
-// )
-// }
+  function Productos (props) {
+    const {carrito} = props;
 
-function Productos (props) {
-  const {carrito} = props;
+    return(
 
-  return(
-
-  <List.Item>
-    <div className="d-none d-lg-block px-2">
-      <img
-        width={272}
-        src={`https://prueba-imagenes-uploads.s3.us-west-1.amazonaws.com/${carrito.idarticulo[0].imagen}`}
-      />
-    </div>
-
-    <div>
-      <div className="d-flex justify-content-lg-center">
-        <List.Item.Meta
-          avatar={<img src={carrito.idarticulo[0].imagen} 
-          className="d-block d-sm-block d-lg-none"  height="70" width="70"/>}
-          title={carrito.idarticulo[0].nombre}
+    <List.Item className="carritoList">
+      
+      <div className="col-lg-3 d-none d-lg-block ">
+        <img
+          width={180}
+          height={160}
+          src={`https://prueba-imagenes-uploads.s3.us-west-1.amazonaws.com/${carrito.idarticulo.imagen}`}
         />
       </div>
 
-      <div className="d-flex mt-3">
-      {carrito.producto.categoria === 'calzado' ? (
+      <div className="col-lg-6">
+        <div className="d-flex justify-content-lg-center" style={{fontSize: 30}} >
+          <List.Item.Meta style={{fontSize: 30}}
+            avatar={<img src={`https://prueba-imagenes-uploads.s3.us-west-1.amazonaws.com/${carrito.idarticulo.imagen}`} 
+              className="d-block d-sm-block d-lg-none"  height="70" width="70"/>}
+
+            title={<p className="carritoList__titulo">{carrito.idarticulo.nombre}</p>}
+          />
+        </div>
+
+        {/* <div className="d-flex mt-3">
+        {carrito.producto.categoria === 'calzado' ? (
+            <p>
+            
+          </p>
+          ): carrito.producto.categoria === 'ropa' ? (
           <p>
           
-        </p>
-        ): carrito.producto.categoria === 'ropa' ? (
-        <p>
-        
-        </p>
-        ) : '' }
-      </div>
+          </p>
+          ) : '' }
+        </div> */}
 
-      <div className="mt-3 d-flex">
-        <p><Link to="/">
-          <ShoppingCartOutlined style={styles}/>Comprar ahora </Link> </p>
-        <p><Link to="/" className="px-3">
-          <DeleteOutlined style={styles}/>Eliminar producto</Link></p>
-        <p><Link  to="/" className="px-3">
-          <ExportOutlined style={styles}/>Apartar</Link></p>
+        <div className="mt-3 d-flex">
+          <p><Link to="/">
+            <ShoppingCartOutlined style={styles}/>Comprar ahora </Link> </p>
+          <p><Link to="/" className="px-3">
+            <DeleteOutlined style={styles}/>Eliminar producto</Link></p>
+          <p><Link  to="/" className="px-3">
+            <ExportOutlined style={styles}/>Apartar</Link></p>
+          </div>
         </div>
-      </div>
-    
-      <div className="mt-4 align-items-center px-5">
-        <InputNumber size="large" min={1} max={100}/>
-        <p>Stock disponibles</p>
-      </div>
+      
+        <div className="col-lg-2 mt-4 ">
+          <InputNumber size="large" min={1} max={100}/>
+          <p>Stock disponibles</p>
+        </div>
 
-      <div className="mt-4 ">
-        <h1>$10000</h1>
-      </div>  
-                
-  </List.Item>
+        <div className="col-lg-1 mt-4">
+          <h1>{carrito.idarticulo.precio}</h1>
+        </div>  
+                  
+    </List.Item>
 
-  )
-}
+    )
+
+  }
--- src/pages/users/Carrito/shopping_cart.js
+++ src/pages/users/Carrito/shopping_cart.js
@@ -15,35 +15,18 @@ export default function ShoppingCart() {
       <div className="container" >
         <h1 className="text-center">Tu carrito de compras</h1>
 
-        <div  className="row mx-auto" style={{background: "white"}}>
-          <div className="col-lg-12 ">
-            
-          <MostrarDatosProductos ></MostrarDatosProductos>
+        <div className="row mx-auto" style={{background: "white"}}>
 
-
-            <div className="d-flex flex-row-reverse mt-22" >
-              <div>
-                <h2>MilOchoMil</h2>
-                <h2>Gratis</h2>
-                <h1>$1001</h1>
-                <br/><br/>
-              </div>
-              <div className="px-5 ">
-                <h2>Productos () </h2>
-                <Link to="/"><h2>Envio a: </h2></Link>
-                <h2 className="mt-4">Total: </h2>
-              </div>
-
-            </div>
-
-            <div className="d-flex flex-row-reverse pb-3" >
+          <div>
+            <MostrarDatosProductos ></MostrarDatosProductos>
+          
+            <div className="d-flex flex-row-reverse pb-3 mr-5" >
               <Button size="large" type="primary" style={{width: 250, textAlign: "center"}}> 
                 <ShoppingCartOutlined style={styles}/> Comprar ahora
               </Button>
             </div>
-
           </div>
-
+          
         </div>
       </div>
     )
