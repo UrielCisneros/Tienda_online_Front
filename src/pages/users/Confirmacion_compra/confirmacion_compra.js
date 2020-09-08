47669cbb4729e9874341fc5e216c0072f734cc55
poday98 <poday736.98@gmail.com>
2020-09-08 01:35:21 -0500

    enlaces

---
+++ src/pages/users/Confirmacion_compra/confirmacion_compra.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import traer_pedido from "./services/traer_pedido"
+import traer_datos from "./services/traer_datos"
+
+import "./confirmacion.scss";
+
+export default function confirmacion_compra() {
+    return (
+        <div>
+        <h1>Termina de realizar tu compra:</h1>
+        <div className="container prinpales">
+            <div className="row">
+                <div className="col-lg-6">
+                    <h2 className="__titulos">Verifica que tus datos esten correctos:</h2>
+                    <traer_datos></traer_datos>
+                </div>
+
+                <div className="col-lg-6">
+                    <h2 className="__titulos">Tu pedido:</h2>
+                    <traer_pedido></traer_pedido>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
---
+++ src/pages/users/Confirmacion_compra/services/traer_datos.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import clienteAxios from '../../../../config/axios';
+
+export default function traer_datos() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
