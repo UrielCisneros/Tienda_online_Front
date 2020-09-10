090477a5130dc039f21b3146b594551230f7a347
AldoChagollan <aldochagollan@outlook.com>
2020-09-10 10:09:18 -0500

    navbar de categorias

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import Navegacion from '../components/Navegacion/Navegacion';
+import Categorias from './Categorias/Categorias';
 import FooterPage from '../components/Footer/Footer';
 import './Layout.scss';
 
@@ -14,6 +15,7 @@ export default function LayoutBasic(props) {
 			<Layout>
 				<Layout>
 					<Navegacion />
+					<Categorias />
 					<Content style={{ height: "auto" }}>
 						<div className="site-layout-content flex">
 							<LoadRoutes routes={routes} />
