591c07eaf00de06b2d848bd4d102dfc0cd66ecca
AldoChagollan <aldochagollan@outlook.com>
2020-10-06 15:32:34 -0500

    build

---
+++ build/precache-manifest.e255de002277cd913835f286bda0a806.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cfef5a2c2b9b0e4e95c66bc73285bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1929387d0aa807ac3106",
+    "url": "./static/css/2.5c87a850.chunk.css"
+  },
+  {
+    "revision": "f0b58f833a59e6239983",
+    "url": "./static/css/main.1e2b0ece.chunk.css"
+  },
+  {
+    "revision": "1929387d0aa807ac3106",
+    "url": "./static/js/2.c10c2471.chunk.js"
+  },
+  {
+    "revision": "a9d318d174af6c4acafeb85268f12ec2",
+    "url": "./static/js/2.c10c2471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b58f833a59e6239983",
+    "url": "./static/js/main.98b72559.chunk.js"
+  },
+  {
+    "revision": "8a50979fe23956a8f580",
+    "url": "./static/js/runtime-main.1a525371.js"
+  },
+  {
+    "revision": "3c6258e568480fc1e8bd769e42d7579c",
+    "url": "./static/media/bg-tarjeta-01.3c6258e5.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "./static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "ae0915fe18dcc1bafdd686a15e448ed6",
+    "url": "./static/media/mastercard.ae0915fe.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "./static/media/visa.d2eb00b7.png"
+  }
+]);
