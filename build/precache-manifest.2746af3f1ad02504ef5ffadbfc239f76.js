6f1fd84415b3a3aaf5bbee5e135e0861bf822629
AldoChagollan <aldochagollan@outlook.com>
2020-10-12 13:19:47 -0500

    preloading principal

---
+++ build/precache-manifest.2746af3f1ad02504ef5ffadbfc239f76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7a8ac148aed2c4f906601c035fb082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e08a3fcd7e9cc21dab2",
+    "url": "/static/css/2.5c87a850.chunk.css"
+  },
+  {
+    "revision": "621c50851bf24d0ad075",
+    "url": "/static/css/main.89e51196.chunk.css"
+  },
+  {
+    "revision": "5e08a3fcd7e9cc21dab2",
+    "url": "/static/js/2.ab178c2e.chunk.js"
+  },
+  {
+    "revision": "a9d318d174af6c4acafeb85268f12ec2",
+    "url": "/static/js/2.ab178c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621c50851bf24d0ad075",
+    "url": "/static/js/main.36ba5393.chunk.js"
+  },
+  {
+    "revision": "e621ac90fd4b80177548",
+    "url": "/static/js/runtime-main.5aecc905.js"
+  },
+  {
+    "revision": "3c6258e568480fc1e8bd769e42d7579c",
+    "url": "/static/media/bg-tarjeta-01.3c6258e5.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "/static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "ae0915fe18dcc1bafdd686a15e448ed6",
+    "url": "/static/media/mastercard.ae0915fe.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "/static/media/visa.d2eb00b7.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89e22b82689f84b13c2ed72127c687c3.js"
+  "/precache-manifest.2746af3f1ad02504ef5ffadbfc239f76.js"
 );
 
 self.addEventListener('message', (event) => {
