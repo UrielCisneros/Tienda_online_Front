b23d2ffead49da101a76c67f4aad969a53ecaec4
AldoChagollan <aldochagollan@outlook.com>
2020-10-14 10:14:55 -0500

    loading

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import { MenuProvider } from '../../context/carritoContext';
